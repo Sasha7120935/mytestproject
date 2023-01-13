eb243b8217cbf3f089187acb06af439bf78c955b
Sasha7120935 <belousalek@gmail.com>
2023-01-13 23:58:28 +0200

    all methods plugin

---
+++ TestPlugin/Cli/Plugin/Generation.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace TestPlugin\Cli\Plugin;
+
+class Generation
+{
+
+// example method after
+
+//    public function afterConfigure(\TestPlugin\Cli\Model\Generation $subject, $result)
+//    {
+//        $title = "gen:cl";
+//        $subject->setName($title);
+//
+//        return $result;
+//    }
+
+    // example method around
+
+//    public function aroundConfigure(\TestPlugin\Cli\Model\Generation $subject, $proceed)
+//    {
+//        $subject->setName(' generation:clean');
+//        if (!empty($subject)) {
+//            return false;
+//        }
+//        return $proceed;
+//    }
+
+  // example method before
+//    public function beforeConfigure(\TestPlugin\Cli\Model\Generation $subject)
+//    {
+//        $subject->addArgument('test');
+//    }
+}
